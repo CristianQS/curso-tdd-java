fa4a39553365821e6645deeb7c614518e4cc548c
Fran Mosteiro <fran.mosteiro@holaluz.com>
2021-04-15 10:11:29 +0200

    Green: multiple_of_three_is_Fizz

--- katas-java/fizz-buzz/src/test/java/FizzBuzzTest.java
+++ katas-java/fizz-buzz/src/test/java/FizzBuzzTest.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -5,7 +6,10 @@ import org.junit.jupiter.api.Test;
 public class FizzBuzzTest {
 
 	@Test
-	public void fix_me_and_rename_me() {
-		assertTrue(false);
+	public void multiple_of_three_is_Fizz() {
+
+		var result = FizzBuzz.ConvertToFizzBuzz();
+
+		assertEquals("Fizz", result.get(2));
 	}
 }
